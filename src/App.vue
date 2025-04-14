f291c619fc77992d8a72b07018d748d990d7ced5
Johannes Schweig <j.schweig@posteo.de>
2025-04-14 13:48:11 +0200

    header, images

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Avatar from './components/Avatar.vue'
 </script>
 
 <template>
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div class="max-w-7xl mx-auto">
+    <div class="flex mt-12 mb-8">
+      <Avatar person="ise"/>
+      <div class="grow"></div>
+      <Avatar person="becci"/>
+    </div>
+    <h1 class="text-6xl mb-8 text-center font-slab">
+      <div class="text-emerald-900">Wipfeltanz</div>
+      <div class="text-orange-900">im</div>
+      <div class="text-fuchsia-900">Lobetal</div>
+    </h1>
+    <div class="flex mb-8">
+      <Avatar person="lotta"/>
+      <div class="grow">]
+        <div class="mb-8 text-lg">
+          <p class="text-center font-bold text-stone-800">25.07 - 27.07</p>
+          <p class="text-center font-bold text-stone-800 hover:text-stone-600">
+            <a href="https://maps.app.goo.gl/LyBYxpTkdrQuBfxA6">Lobetal bei Bernau</a>
+          </p>
+        </div>
+      </div>
+      <Avatar person="florenz"/>
+    </div>
 
+  </div>
 </template>
---
+++ src/components/Avatar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="">
+    <img class="max-w-24 max-h-24" :src="imageSrc" />
+    <div class="text-center font-slab text-stone-700">{{ name }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps({
+  person: {
+    type: String,
+    required: true
+  }
+})
+
+const imageSrc = `/src/assets/${props.person}.png`
+
+const name = computed(() => {
+  switch(props.person) {
+    case 'ise':
+      return 'Ise'
+    case 'becci':
+      return 'Becci'
+    case 'lotta':
+      return 'Lotta'
+    case 'florenz':
+      return 'Florenz'
+    default:
+      return ''
+  }
+})
+</script>
