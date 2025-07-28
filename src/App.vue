520f5bf77dcd7a4af5ee2a67614bd1f75a02da59
Johannes Schweig <j.schweig@posteo.de>
2025-07-28 13:12:19 +0200

    thanks message

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,11 @@
 import Tabbar from './components/Tabbar.vue'
 import Hero from './components/Hero.vue'
 import Welcome from './components/Welcome.vue'
+import Thanks from './components/Thanks.vue'
 </script>
 
 <template>
   <div class="max-w-3xl mx-auto">
-    <!-- Hero -->
-    <Hero />
-    <!-- Welcome -->
-    <Welcome />
-    <Tabbar />
-    <!-- Details -->
-    <div class="mx-3 md:mx-6">
-      <router-view />
-    </div>
+    <Thanks />
   </div>
 </template>
---
+++ src/components/Thanks.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mt-8 mx-4">
+    <h1 class="text-5xl md:text-6xl mb-8 text-center font-slab">
+      <div class="text-emerald-900 mb-4">Danke</div>
+    </h1>
+    <img src="@/assets/bye.jpg" class="rounded-2xl mb-4" />
+    <p>
+      Habt ganz lieben Dank, dass ihr alle da wart. Für die wunderbaren Workshops, tollen Gespräche, gute Zeit auf dem
+      Dancefloor und für eure gigantische Mithilfe! Es war wirklich ein Fest mit euch ❤️
+    </p>
+    <p class="float-right italic">
+      Florens, Lotta, Bekki und Ise
+    </p>
+  </div>
+</template>
